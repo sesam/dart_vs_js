71fcc2e806d253885f709aa62595aca258ae9123
Jacob Leverich <leverich@gmail.com>
2014-07-16 18:52:31 -0700

    Added comparison to more idiomatic usage of Scala.

---
+++ comparisons/functions/functions/idiomatic.scala
@@ -0,0 +1,4 @@
+def greet(name: String, day: String): String =
+    s"Hello $name, today is $day."
+
+greet("Bob", "Tuesday")
---
+++ comparisons/functions/tuple_return/idiomatic.scala
@@ -0,0 +1,2 @@
+def getGasPrices(): (Double, Double, Double) =
+    (3.59, 3.69, 3.79)
